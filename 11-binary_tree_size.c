64be973de25eca552761ced6b9c0cf4b959c1c04
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-30 10:32:53 -0800

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -5,6 +5,7 @@
  * @tree: is a pointer to the root node of the tree to measure the size of.
  * Return: The size of the tree.
  **/
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0;
