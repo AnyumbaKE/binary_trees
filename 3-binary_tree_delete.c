7405bfe59568634285876f44e9ac926bce233083
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-30 11:13:07 -0800

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,6 +4,7 @@
  * binary_tree_delete - deletes an entire binary tree
  * @tree: is a pointer to the root node of the tree to delete
  **/
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
