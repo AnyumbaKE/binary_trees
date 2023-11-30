e1f1efe3da2962bef3a991af8e6172e03a8347eb
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-30 11:02:55 -0800

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,6 +6,7 @@
  * @value:  is the value to store in the new node
  * Return: pointer to the created node, or NULL on failure or if parent is NULL
  **/
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root;
