00d08b7c1d147e748be619f962dd25b0c0b9065e
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-30 10:03:05 -0800

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,6 +6,7 @@
  * @value:  is the value to put in the new node
  * Return: a pointer to the new node, or NULL on failure
  **/
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root;
