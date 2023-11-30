32d4641e3879b8453ee0a107197849c47a0e3864
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-30 10:54:13 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,6 +5,7 @@
  * @tree: a pointer to the root node of the tree to measure the balance factor.
  * Return: If tree is NULL, return 0, else return balance factor.
  **/
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
@@ -18,6 +19,7 @@ int binary_tree_balance(const binary_tree_t *tree)
  * @tree: a pointer to the root node of the tree to measure the height.
  * Return: If tree is NULL, your function must return 0, else return height.
  **/
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
