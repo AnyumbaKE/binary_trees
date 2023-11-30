2c9244e76ff99ee99fa8e68b50d7fbb3379c252c
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-30 11:00:00 -0800

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,6 +7,7 @@
  * Return: If node is NULL or there is no sibling - NULL.
  *         Otherwise - a pointer to the sibling.
  **/
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
