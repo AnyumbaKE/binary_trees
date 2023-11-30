d7ad674855715efed6c6afb0536b9f2415edb2e9
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-30 11:22:27 -0800

    5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,6 +5,7 @@
  * @node: is a pointer to the node to check
  * Return: 1 if node is a leaf, otherwise 0
  **/
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent != NULL)
